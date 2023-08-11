9264969cf568268a4c6b91e2da16b75e1e0e0679
kapi104 <skorupa7979@gmail.com>
2023-08-11 17:14:32 +0200

    added winner check, needs debugging

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const module = (() => {
   let currentPlayer
   let test = true;
   
-  const board = [];
+  let board = [];
 
   const generateBoard = function()  {
     for (let i = 0; i < 3; i++) {
@@ -13,17 +13,24 @@ const module = (() => {
         field.classList.add('field');
         field.dataset.row = i; 
         field.dataset.column = j; 
-        
         document.querySelector('.gameField').appendChild(field);
       }
-
   }
   }();
 
-  
-  
+  const resetBoard = () => {
+    module.board = [];
+    for (let i = 0; i < 3; i++) {
+      module.board[i] = [];
+      for(let j = 0; j < 3; j++) {
+        module.board[i].push(0);
+      }
+  }
+  }
   
   const gameController = () => {
+
+    let fields
     
     const players = [
       playerOne = player('Player one', 'X'),
@@ -50,16 +57,18 @@ const module = (() => {
     }
 
     const getFields = () => {
-      return document.querySelectorAll('.field');
+      fields = document.querySelectorAll('.field');
+      return fields;
     }
 
     const clickListener = function () {
-      const fields = getFields();
+      getFields()
 
       fields.forEach(field => {
         field.addEventListener('click', clickEvent, false);
       })
     }
+
     const clickEvent = (e) => {
       const field = e.target
       let cords = {
@@ -68,20 +77,70 @@ const module = (() => {
       }
       
       changeMark(cords.row, cords.column, currentPlayer.getMark())
-      module.gameController().switchPlayers()
       document.querySelector('h3').innerText = 'turn: ' + currentPlayer.getMark()
     }
-
+    
     
     const changeMark = (row, column, turn) => {
-      if (module.board[row][column] === 0) {
+      if (module.board[row][column] == 0) {
         let targetRow = Array.from(document.querySelectorAll(`[data-row="${row}"]`));
         
         targetRow.forEach(field => {
           if (field.dataset.column == column) field.innerHTML = turn;
+          module.board[row][column] = turn;
         })
         
+        setTimeout(() => {
+          
+          checkBoard()
+        }, 1);
+        module.gameController().switchPlayers()
+        
       } 
+    }
+
+
+    const checkBoard = () => {
+      const equalArr = arr => arr.every( v => v === arr[0] && v !== 0)
+      let rowResult = [];
+      let columnResult = [];
+      let diagResult = []
+      for(let i = 0; i < 3; i++) {
+        rowResult[i] = equalArr(module.board[i])
+      }
+      console.log({rowResult});
+
+      for(let i = 0; i < 3; i ++) {
+        let column = [];
+        for(let j = 0; j < 3; j ++) {
+          column.push(module.board[j][i]);
+        }
+        columnResult[i] = equalArr(column)
+      }
+      console.log({columnResult});
+
+      let rl = [];
+      for(let i = 0; i < 3; i ++) {
+        rl.push(module.board[i][i]) 
+      }
+      diagResult[0] = equalArr(rl);
+
+      let lr = []
+      let temp = 2;
+      for(let i = 0; i < 3; i ++) {
+        lr.push(module.board[i][temp]) 
+        temp -= 1;
+      }
+      diagResult[1] = equalArr(lr);
+
+      checkWinner(rowResult, columnResult, diagResult);
+    }
+
+    const checkWinner = (row, column, diag) => {
+      const containTrue = arr => arr.includes(true);
+      if (containTrue(row) || containTrue(column) || containTrue(diag)) {
+        alert('wygrywa' + currentPlayer.getMark())
+      }
     }
 
     return {players, clickListener, switchPlayers, getCurrentPlayer, getFields}
@@ -92,6 +151,7 @@ const module = (() => {
     document.querySelector('button').addEventListener('click', () => {
       let fields = gameController().getFields();
       fields.forEach(field => field.innerHTML = '');
+      resetBoard();
     }, false)
   }
 
